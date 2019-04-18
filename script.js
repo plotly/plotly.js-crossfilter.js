b3143bb65c078faac3f980be810b105c14c11731
Nicolas Kruchten <nicolas@datacratic.com>
2019-04-18 15:38:00 -0400

    finishing react version

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-var resetFilters = null;
-
 Papa.parse("data.csv", {
   download: true, skipEmptyLines: true, dynamicTyping: true, header: true,
   complete: function(parsed){
-
-    //Helper
-    function unpack(rows, key) {
-        return rows.map(function(row) { return row[key]; });
-    }
 
     //Crossfilter dimension and group setup
     var data = crossfilter(parsed.data),
@@ -23,13 +16,17 @@ Papa.parse("data.csv", {
       hist_gdp = document.getElementById("hist_gdp"),
       map = document.getElementById("map");
 
-    //save selected ranges, initialized to 'everything'
-    var im_range =[-Infinity, Infinity],
-      gdp_range =[-Infinity, Infinity],
-      country_range = [];
+    // colors
+    var in_all = "#66F",
+        in_none = "#EEE",
+        in_here = "#CCC",
+        in_there = "#CCF";
 
-    //create plots with placeholder series
 
+    //helpers
+    function unpack(rows, key) {
+      return rows.map(function(row) { return row[key]; });
+    }
 
     function getXY(arr) {
       return { x: unpack(arr, "key"), y: unpack(arr, "value") }
@@ -52,16 +49,15 @@ Papa.parse("data.csv", {
       }]
     }
 
-    var all_im = getXY(ims.all());
-    var all_gdp = getXY(gdps.all());
+    //save selected ranges, initialized to 'everything'
+    var im_range =[-Infinity, Infinity],
+      gdp_range =[-Infinity, Infinity],
+      country_range = [],
+      all_im = getXY(ims.all()),
+      all_gdp = getXY(gdps.all());
 
-    var in_all = "#66F",
-        in_none = "#EEE",
-        in_here = "#CCC",
-        in_there = "#CCF";
-
-    //define redraw to call on crossfilter
-    function redraw() {
+    //define charts
+    function react() {
       Plotly.react(hist_im, [
         makeBars(all_im, im_range, in_there, in_none),
         makeBars(getXY(ims.all()), im_range, in_all, in_here)
@@ -72,7 +68,7 @@ Papa.parse("data.csv", {
         width: 450, height: 300, margin: {r:20,b:40,t:80,l:40}, selectdirection: "h",
         barmode: "overlay", hovermode: false, showlegend: false, dragmode: "select",
         shapes: makeShapes(im_range)
-      })
+      });
 
       Plotly.react(hist_gdp, [
         makeBars(all_gdp, gdp_range, in_there, in_none),
@@ -84,12 +80,12 @@ Papa.parse("data.csv", {
         width: 450, height: 300, margin: {r:20,b:40,t:80,l:40}, selectdirection: "h",
         barmode: "overlay", hovermode: false, showlegend: false, dragmode: "select",
         shapes: makeShapes(gdp_range)
-      })
+      });
 
       Plotly.react(map, [{
-        type: 'choropleth',
-        locationmode: 'ISO-3',
+        type: 'choropleth', locationmode: 'ISO-3',
         locations: unpack(countries.all(), "key"),
+        hovertemplate: "%{location}<extra></extra>",
         z: countries.all().map(function(d){
           if(d.value == 0) { // excluded here
             if(country_range.length == 0) return 0.25;
@@ -106,31 +102,32 @@ Papa.parse("data.csv", {
       {
         dragmode: "lasso", margin: {r:0,b:0,t:0,l:0}, height: 250,
         geo: { projection: { type: 'robinson' } }
-      })
+      });
     }
 
-    //do the initial draw with no filters
-    redraw();
+    // react once to create the charts for event subscription
+    react();
 
     //set up selection listeners
     function hist_im_select(e) {
       im_range = e ? [e.range.x[0], e.range.x[1]] : [-Infinity, Infinity];
       im.filter(im_range);
-      redraw();
+      react();
     }
 
-    hist_im.on('plotly_selected', hist_im_select);
+    hist_im.on('plotly_selected',    hist_im_select);
     hist_im.on('plotly_doubleclick', hist_im_select);
-    hist_im.on('plotly_selecting', hist_im_select);
+    hist_im.on('plotly_selecting',   hist_im_select);
 
     function hist_gdp_select(e) {
       gdp_range = e ? [e.range.x[0], e.range.x[1]] : [-Infinity, Infinity];
       gdp.filter(gdp_range);
-      redraw();
+      react();
     }
-    hist_gdp.on('plotly_selected',  hist_gdp_select);
-    hist_gdp.on('plotly_doubleclick',  hist_gdp_select);
-    hist_gdp.on('plotly_selecting', hist_gdp_select);
+
+    hist_gdp.on('plotly_selected',    hist_gdp_select);
+    hist_gdp.on('plotly_doubleclick', hist_gdp_select);
+    hist_gdp.on('plotly_selecting',   hist_gdp_select);
 
     function map_select(e) {
       if(e && e.points.length != 0) {
@@ -141,22 +138,16 @@ Papa.parse("data.csv", {
         country_range = []
         country.filterAll();
       }
-      redraw();
+      react();
     }
 
     map.on('plotly_selected',  map_select);
     map.on('plotly_selecting', map_select);
 
-    resetFilters = function() {
-      gdp_range = [-Infinity, Infinity];
-      im_range = [-Infinity, Infinity];
-      country_range = [];
-      im.filter(im_range);
-      gdp.filter(gdp_range);
-      country.filterAll();
-      redraw();
+    window.resetFilters = function() {
+      hist_gdp_select();
+      hist_im_select();
+      map_select();
     };
-
-
   }
 });
